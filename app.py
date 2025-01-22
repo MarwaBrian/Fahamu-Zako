4c463fc4f02c6ab6fbbaf3bdf2588f0255d6c0c5
Brian Chacha <101217241+Marwa254@users.noreply.github.com>
2025-01-22 22:41:51 +0300

    logout logic added

--- app.py
+++ app.py
@@ -207,8 +207,33 @@ def register():
             flash(f'An error occurred. {str(e)} Please try again.', 'danger')
     return render_template('register.html', title='Register', form=form)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('app.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('app.home'))
+        else:
+            flash('Login unsuccessful. Please check your email and password.', 'danger')
 
+    return render_template('login.html', title='Login', form=form)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('app.home'))
+
+@app.route('/my_account')
+@login_required
+def my_account():
+    return render_template('my_account.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
