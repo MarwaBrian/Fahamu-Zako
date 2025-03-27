edff0d6906ce38561c15ea4eda8a1a214db64d8c
chemiatsalome <chemiatsalome@gmail.com>
2025-03-27 20:02:31 +0300

    Worked on the requirements file

--- app.py
+++ app.py
@@ -2,28 +2,26 @@ from flask import Flask, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db  # Import db from models/__init__.py
-from models.models import User # Import models
+from models.models import User  # Import models
 
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Allow 
+CORS(app)  # Allow cross-origin requests
 
 app.config.from_object('config.Config')
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
-
 # Import models to register them
 with app.app_context():
     from models.models import *
 
-
+# Import routes
 from routes.authroutes import Login_bp, signup_bp
 from AI_model.modelIntegration import chat_bp
 from routes.approutes import approutes_bp
-
 
 # Register Blueprints (modular routes)
 app.register_blueprint(Login_bp)
@@ -31,12 +29,8 @@ app.register_blueprint(signup_bp)
 app.register_blueprint(approutes_bp)
 app.register_blueprint(chat_bp, url_prefix="/chat")  # Register the blueprint
 
+# Use waitress to serve the app
+from waitress import serve
 
-# print(app.url_map)
-
-import nltk
-nltk.download('all')  # Or specific corpora like 'punkt', 'wordnet', etc.
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', port=8000)
